1922b7fb1ba9779aeaf46daadb021f3fa6cf50d0
vjosullivan <vjosullivan@hotmail.com>
2020-01-25 12:15:54 +0000

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Vincent O'Sullivan on 25/01/2020.
+//  Copyright © 2020 Vincent O'Sullivan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
